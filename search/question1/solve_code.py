7234d11f8b9380a5322d9006af023ebda803ccac
dev-msj <msj944@naver.com>
2025-11-19 18:52:18 +0900

    탐색 - 실전 문제1 상세 설명 추가

--- search/question1/solve_code.py
+++ search/question1/solve_code.py
@@ -3,32 +3,35 @@ import pytest
 
 def solution(size: str, ice_box: list[str]) -> int:
     n, m = map(int, size.split())
-    graph = []
+    ice_box_grid = []
     for i in range(n):
         print(i, ice_box[i])
-        graph.append(list(map(int, ice_box[i])))
+        ice_box_grid.append(list(map(int, ice_box[i])))
     
-    def dfs(x, y):
-        # 주어진 범위를 벗어나면 종료
+    def dfs(x: int, y: int) -> bool:
+        # 격자 범위를 벗어나면 탐색 종료
         if x <= -1 or x >= n or y <= -1 or y >= m:
             return False
         
-        # 방문하지 않은 노드면 방문 처리 후 상하좌우 탐색
-        if graph[x][y] == 0:
-            graph[x][y] = 1
+        # 방문하지 않은 노드(구멍 뚫린 칸)이면 방문 처리 후 상하좌우 탐색
+        if ice_box_grid[x][y] == 0:
+            # 방문 처리
+            ice_box_grid[x][y] = 1
             
+            # 인접 노드 탐색
             dfs(x - 1, y)
             dfs(x, y - 1)
             dfs(x + 1, y)
             dfs(x, y + 1)
             
-            # 탐색이 완료되면 카운트
+            # 탐색이 완료되면 아이스크림 카운트
             return True
         
-        # 방문하지 않은 노드가 없으므로 카운팅하지 않음
+        # 이미 방문했거나 칸막이 노드(1)인 경우 카운트하지 않음
         return False
     
     result = 0
+    # 격자 노드의 모든 좌표를 직접 순회하며 dfs 수행
     for i in range(n):
         for j in range(m):
             if dfs(i, j) == True:
